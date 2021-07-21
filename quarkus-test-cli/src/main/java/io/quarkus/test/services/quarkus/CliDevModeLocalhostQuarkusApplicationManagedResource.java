d696fa58dba90b3fd4c7e0f797aa191a152650c7
Jose <josecarvajalhilario@gmail.com>
2021-07-21 12:42:53 +0200

    CLI: Use io.quarkus as platform group id

--- quarkus-test-cli/src/main/java/io/quarkus/test/services/quarkus/CliDevModeLocalhostQuarkusApplicationManagedResource.java
+++ quarkus-test-cli/src/main/java/io/quarkus/test/services/quarkus/CliDevModeLocalhostQuarkusApplicationManagedResource.java
@@ -18,6 +18,9 @@ import io.quarkus.test.utils.SocketUtils;
 public class CliDevModeLocalhostQuarkusApplicationManagedResource extends QuarkusManagedResource {
 
     private static final String QUARKUS_HTTP_PORT_PROPERTY = "quarkus.http.port";
+    private static final String QUARKUS_PLATFORM_GROUP_ID = "quarkus.platform.group-id";
+    private static final String QUARKUS_PLATFORM_GROUP_ID_VALUE = "io.quarkus";
+    private static final String QUARKUS_PLATFORM_VERSION = "quarkus.platform.version";
 
     private final ServiceContext serviceContext;
     private final QuarkusCliClient client;
@@ -89,6 +92,8 @@ public class CliDevModeLocalhostQuarkusApplicationManagedResource extends Quarku
     private Map<String, String> getPropertiesForCommand() {
         Map<String, String> runtimeProperties = new HashMap<>(serviceContext.getOwner().getProperties());
         runtimeProperties.putIfAbsent(QUARKUS_HTTP_PORT_PROPERTY, "" + assignedHttpPort);
+        runtimeProperties.putIfAbsent(QUARKUS_PLATFORM_GROUP_ID, QUARKUS_PLATFORM_GROUP_ID_VALUE);
+        runtimeProperties.putIfAbsent(QUARKUS_PLATFORM_VERSION, io.quarkus.builder.Version.getVersion());
 
         return runtimeProperties;
     }
